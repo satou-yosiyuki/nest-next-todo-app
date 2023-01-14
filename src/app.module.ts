20ce85222137e06488e396a74c84fd3ce53c68c5
satou-yosiyuki <stuysyk1027@icloud.com>
2023-01-15 02:57:27 +0900

    prismaの導入

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemModule } from './item/item.module';
+import { ItemsController } from './items/items.controller';
 
 @Module({
   imports: [ItemModule],
-  controllers: [AppController],
+  controllers: [AppController, ItemsController],
   providers: [AppService],
 })
 export class AppModule {}
