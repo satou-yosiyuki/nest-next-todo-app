0e0e27c8bf5aad35d7071248e55f78eddea4e391
satou-yosiyuki <stuysyk1027@icloud.com>
2023-01-21 00:00:22 +0900

    コントローラー、モジュールの作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemModule } from './item/item.module';
 import { ItemsController } from './items/items.controller';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { TodoModule } from './todo/todo.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ItemModule],
+  imports: [ItemModule, AuthModule, UserModule, TodoModule, PrismaModule],
   controllers: [AppController, ItemsController],
   providers: [AppService],
 })
